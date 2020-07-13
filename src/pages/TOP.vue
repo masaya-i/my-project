7547315d9dff73304204a1c134a5b6616ac89ab2
masaya-i <kiyozonbi@gmail.com>
2020-07-13 17:58:13 +0900

    i

--- src/pages/TOP.vue
+++ src/pages/TOP.vue
@@ -7,7 +7,7 @@
       <div class="columns is-centered">
         <div class="column is-10">
           <br>
-          <img class="gif" src="https://github.com/masaya-i/test/blob/master/img/TOP.jpg?raw=true" alt="画像を表示出来ません">
+          <img class="gif" src="./assets/TOP.jpg" alt="画像を表示出来ません">
         </div>
       </div>
     </div>
